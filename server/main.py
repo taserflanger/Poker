1cfb3480445de17fdbbabd48f284d82e23ce4c98
pascal <pascal.nardi@psl.eu>
2020-10-26 14:25:42 +0100

    communication établie

--- server/main.py
+++ server/main.py
@@ -1,22 +1,26 @@
 import socketio
 import eventlet
 
-sio = socketio.Server()
+sio = socketio.Server(cors_allowed_origins="*")
 app = socketio.WSGIApp(sio, static_files={
     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 })
 
 
 @sio.on('connect')
-def connect(sid, environ):
-    print('connect ', sid)
+def connect(sid, e):
+    print('client connecté')
+
+
+@sio.on('msg')
+def msg(sid, m):
+    print(m)
 
 
 @sio.on('disconnect')
 def disconnect(sid):
-    print('disconnect ', sid)
+    print('client déconnecté')
 
 
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
-
+    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
