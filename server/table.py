402f1876e781b54bbfa7581c34bd833773ac858d
pascal <pascal.nardi@psl.eu>
2020-10-31 19:20:40 +0100

    merge rodrigue-pascal

--- server/table.py
+++ server/table.py
@@ -1,23 +1,33 @@
+from deck import Deck
+
 """
 L'itérateur de la classe table permet de parcourir les joueurs restant dans la main, à partir de celui qui doit parler
 en premier.
 Pour jouer une main:
 
-TODO:   - s'occuper de la fonctionnalité fold, et de l'attribut ind_speaker
-        - s'occuper du cas de figure ou tout le monde sauf 1 s'est couché
-        - retranscrire dans un modèle client/serveur, où le client n'a accès qu'aux informations qui le concernent
+TODO:   - retranscrire dans un modèle client/serveur, où le client n'a accès qu'aux informations qui le concernent
         - (détail: Si un joueur n'a pas assez d'argent pour faire sa blinde, il doit être exclu au début)
 
 """
 import random
 from deck import Deck
 from hand_5 import Hand_5  ### RODRIGUE ###
-from itertool import combinations  ### RODRIGUE ### pour les combinaisons possibles de mains de 5 cartes
+from itertools import combinations  ### RODRIGUE ### pour les combinaisons possibles de mains de 5 cartes
 import random_functions as r_f
+
+# Todo: pour l'implémentation des différents pots
+#       idée: amount to call est une liste qui correspond à chaque pot. Les pots sont ordonnés
+#       par ordre croissant de amount_to_call. Lorsqu'un nouveau joueur se met tapis, on crée
+#       un nouveau pot associé à la valeur du tapis, et on update le pot consécutif
+#       (avec un amount_to_call juste supérieur), aussi, les joueurs associés à ce pot
+#       sont les joueurs du pot suivant + le joueur qui a fait tapis.
+#       On ajoute le joueur qui a fait tapis à tous les pots qui ont un amount_to_call
+#       moins important. (C'est pas clair mais je me comprends)
 
 class Table:
 
-    def __init__(self, table_players, small_blind, big_blind, dealer="random"):
+    def __init__(self, table_players, small_blind, big_blind, dealer="random", mode="high", limit="no-limit",
+                 coin_stacks="default", max_reraise=float("inf")):
         self.nb_players = len(table_players)
         self.players = table_players
         self.nb_players = len(self.players)
@@ -31,8 +41,10 @@ class Table:
         self.bb = big_blind
         self.deck = Deck()
         self.cards = []
-        self.pot = 0
+        self.pots = [0]
+        self.players_in_pots = [list(range(self.nb_players))]
         self.id_dealer = 0  # l'indice du dealer
+        self.last_raiser = 0
 
     def __iter__(self):
         yield self.players[self.id_speaker]
@@ -42,17 +54,19 @@ class Table:
     def new_set(self):
         for player in self.players:
             player.hand = []
+            player.is_all_in = False
             player.final_hand = None  ### RODRIGUE ###
         self.id_dealer = (self.id_dealer + 1) % self.nb_players
         self.id_speaker = (self.id_dealer + 1) % self.nb_players
-        self.pot = 0
+        self.pots = [0]
+        self.players_in_pots = [list(range(self.nb_players))]
         self.cards = []
         self.deck = Deck()
 
-    def next_player_id(self, player_id):
-        next_id = (player_id + 1) % self.nb_players
+    def next_player_id(self, player_id, step=1):
+        next_id = (player_id + step) % self.nb_players
         while not self.active_players[next_id]:
-            next_id = (next_id + 1) % self.nb_players
+            next_id = (next_id + step) % self.nb_players
         return next_id
 
     def initialise_round(self):
@@ -73,14 +87,16 @@ class Table:
         for player in self.players * 2:
             player.hand.append(self.deck.deal())
         for i in range(2):
-            print(f"{self.players[self.id_speaker].name} bets {[self.sb, self.bb][i]}")
-            self.players[self.id_speaker].on_going_bet += [self.sb, self.bb][i]
+            blind = [self.sb, self.bb][i]
+            print(f"{self.players[self.id_speaker].name} bets {blind}")
+            self.players[self.id_speaker].put_on_going_bet(blind)
+            self.last_raiser = self.id_speaker
             self.id_speaker = self.next_player_id(self.id_speaker)
 
     def pre_flop(self):
         print(f'Dealer : {self.id_dealer}', f"Speaker : {self.id_speaker}", sep='\n')
         self.deal_and_blinds()
-        return self.players_speak(self.bb)
+        return self.players_speak()
 
     def flop(self):
         self.initialise_round()
@@ -94,6 +110,10 @@ class Table:
         print([str(card) for card in self.cards])
         return self.players_speak()
 
+
+    def new_pot(self, amount):
+        pass
+
     def players_speak_ro(self, mise=0, raiser=None):
         for active_player in self:
             if active_player == raiser:
@@ -106,26 +126,34 @@ class Table:
             if action == 'f':
                 self.active_players[player_id] = False
 
-    def players_speak(self, mise=0):
+    def players_speak(self):
         # Todo: implémenter le tour depuis le raiser
-        for _ in range(self.nb_players):
+        while True:
             speaker = self.id_speaker
+            previous_speaker = self.next_player_id(speaker, -1)
+            next_speaker = self.next_player_id(speaker)
             if sum(self.active_players) == 1:
-                winner_id = self.next_player_id(
-                    speaker)  # on trouve le dernier joueur restant (qui est aussi le suivant)
+                # cas où il ne reste plus qu'un joueur
+                winner_id = next_speaker  # on trouve le dernier joueur restant (qui est aussi le suivant)
                 return winner_id
-            if not self.active_players[speaker]:
+            if not self.active_players[speaker] or self.players[speaker].is_all_in:
+                # si le joueur est fold on le dégage
                 continue
-            dealer = self.id_dealer
+            amount_to_call = self.players[previous_speaker].on_going_bet
 
-            action, amount = self.players[self.id_speaker].speaks(mise)
-            if speaker == dealer and self.players[self.next_player_id(dealer)].on_going_bet == mise:
+            if speaker == self.last_raiser and \
+                    self.players[speaker].on_going_bet == amount_to_call:
                 # si personne n'a relancé dans le tour, on passe à l'étape suivante.
                 return
+
+            action, amount = self.players[speaker].speaks(amount_to_call)
+            if self.players[speaker].is_all_in:
+                self.new_pot(amount)
             if action == "f":
                 self.active_players[speaker] = False
+            elif action == "r":
+                self.last_raiser = speaker
             self.id_speaker = self.next_player_id(self.id_speaker)
-            mise += amount
 
     def get_winner(self):
         """
@@ -143,4 +171,3 @@ class Table:
                 players_hands.append(player.final_hand)
         winning_hand, winners_ids = r_f.max_with_ids(players_hands)
         return winning_hand, winners_ids
-
