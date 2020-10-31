4034a41287aafcaee17a1e6d37d515e31445ea14
rodrigu2000 <rodrigu2000@gmail.com>
2020-10-31 18:56:55 +0100

    Création de Table.speakers_speak_ro() (il reste des détails à implémenter) ma version de la méthode speakers_speak, à valider ou non. Modification de l'itérateur de classe Table pour aller avec la méthode speakers_speak_ro. Modification de la Table.set(), pour aller avec la méthode speakers_speak_ro

--- server/table.py
+++ server/table.py
@@ -12,7 +12,7 @@ TODO:   - s'occuper de la fonctionnalité fold, et de l'attribut ind_speaker
 import random
 from deck import Deck
 from hand_5 import Hand_5  ### RODRIGUE ###
-from itertools import combinations  ### RODRIGUE ### pour les combinaisons possibles de mains de 5 cartes
+from itertool import combinations  ### RODRIGUE ### pour les combinaisons possibles de mains de 5 cartes
 import random_functions as r_f
 
 class Table:
@@ -31,7 +31,7 @@ class Table:
         self.bb = big_blind
         self.deck = Deck()
         self.cards = []
-        self.pots = [0]
+        self.pot = 0
         self.id_dealer = 0  # l'indice du dealer
 
     def __iter__(self):
@@ -39,17 +39,13 @@ class Table:
         for i in range(sum(self.active_players) - 1):
             yield self.next_player_id()
 
-        i = (self.id_dealer + 1) % self.nb_players
-        for k in range(self.nb_players):
-            yield (i + k) % self.nb_players
-
     def new_set(self):
         for player in self.players:
             player.hand = []
             player.final_hand = None  ### RODRIGUE ###
         self.id_dealer = (self.id_dealer + 1) % self.nb_players
         self.id_speaker = (self.id_dealer + 1) % self.nb_players
-        self.pots = [0]
+        self.pot = 0
         self.cards = []
         self.deck = Deck()
 
@@ -61,7 +57,7 @@ class Table:
 
     def initialise_round(self):
         for player in self.players:
-            self.pots[0] += player.on_going_bet  ### A REVOIR EN PRENANT EN COMPTE LES SIDE POTS
+            self.pot += player.on_going_bet
             player.on_going_bet = 0
         self.id_speaker = self.next_player_id(self.id_dealer)
 
@@ -69,10 +65,9 @@ class Table:
         self.new_set()
         for round_ob in [self.pre_flop, self.flop, self.turn_river, self.turn_river]:
             round_ob()
-            if sum(self.active_players) == 1:
-                self.get_winner()  # le cas où tout le monde s'est couché ou a fait tapis
-        if sum(self.active_players) > 1:
-            self.get_winner()
+            if sum(self.active_players) == 1:  # le cas où tout le monde s'est couché ou a fait tapis
+                break
+        self.get_winner()
 
     def deal_and_blinds(self):
         for player in self.players * 2:
@@ -111,21 +106,19 @@ class Table:
             if action == 'f':
                 self.active_players[player_id] = False
 
-
     def players_speak(self, mise=0):
         # Todo: implémenter le tour depuis le raiser
         for _ in range(self.nb_players):
             speaker = self.id_speaker
-            dealer = self.id_dealer
-
             if sum(self.active_players) == 1:
                 winner_id = self.next_player_id(
                     speaker)  # on trouve le dernier joueur restant (qui est aussi le suivant)
                 return winner_id
             if not self.active_players[speaker]:
                 continue
+            dealer = self.id_dealer
 
-            action, amount = self.players[speaker].speaks(mise)
+            action, amount = self.players[self.id_speaker].speaks(mise)
             if speaker == dealer and self.players[self.next_player_id(dealer)].on_going_bet == mise:
                 # si personne n'a relancé dans le tour, on passe à l'étape suivante.
                 return
@@ -136,7 +129,7 @@ class Table:
 
     def get_winner(self):
         """
-        TODO: la fonction doit faire une liste ordonnée par ordre décroissant de valeur les mains
+        TODO (roro s'en occupe): la fonction doit faire une liste ordonnée par ordre décroissant de valeur des mains
             des joueurs. Ensuite, en bouclant sur chaque pot, elle doit attribuer à chaque fois le pot au meilleur
             joueur de ce pot. Dans cette étape, elle doit prendre en considération des éventuelles égalités.
 
@@ -150,3 +143,4 @@ class Table:
                 players_hands.append(player.final_hand)
         winning_hand, winners_ids = r_f.max_with_ids(players_hands)
         return winning_hand, winners_ids
+
