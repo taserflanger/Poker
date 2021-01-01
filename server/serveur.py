d6cc23e47bf4895ac1ae2719221fba95747e4718
Andros78 <andresbodenheimer@gmail.com>
2021-01-01 20:49:52 +0100

    h

--- server/serveur.py
+++ server/serveur.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 import socket
-import pandas as pd
+#import pandas as pd
 from tournoi import Tournoi
-from cash_game import Cash_game
+#from cash_game import Cash_game
 serveur=socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
 
 adresse_serveur_linode="178.79.165.80"
 adresse_serveur_local=""
 serveur.bind((adresse_serveur_linode, 12800))  
 serveur.listen(5)  
-fichier_data=pd.read_csv("data.csv", sep=",")
+#fichier_data=pd.read_csv("data.csv", sep=",")
 def tournoi(joueur_par_table, stack_initial, sb, bb, nbr_bot):
     tournoi=Tournoi(serveur, joueur_par_table, stack_initial, sb, bb, nbr_bot)
     tournoi.lancer_tournoi()
 
+"""
 def cash_game(joueur_par_table, stack_initial, sb, bb, nbr_bot, data ):
     cash_game=Cash_game(serveur, joueur_par_table, stack_initial, sb, bb, nbr_bot, data)
     cash_game.launch()
+"""
     
 #tournoi(4, 500, 5, 10, 2)
 tournoi(4, 500, 5, 10, 0)
