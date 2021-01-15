7f7230b46a7c2946b42e5169117abe8375a34284
pascal <pascal.nardi@psl.eu>
2021-01-16 00:24:27 +0100

    added sleep 5 sec before launching client

--- scripts/launchgame.bat
+++ scripts/launchgame.bat
@@ -1,4 +1,5 @@
 @set /A PORT = 8888
-start python -m scripts.main_server local %PORT% 0 1&
+start python -m scripts.main_server local %PORT% 0 0&
+ping 127.0.0.1 -n 5 > nul
 start python -m scripts.main_client local %PORT%&
 start python -m scripts.main_client local %PORT%
--- scripts/launchgame.sh
+++ scripts/launchgame.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 port=8888
 python -m scripts.main_server local $port 0 1 &
+sleep 5
 python -m scripts.main_client local $port &
 python -m scripts.main_client local $port &
